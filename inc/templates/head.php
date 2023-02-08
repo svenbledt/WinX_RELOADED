02a448a7c4a3f7ed60c58a2cabdb83c3c7985fe5
Gaudenz@Krakatoom <42372680+Gaudenz77@users.noreply.github.com>
2023-02-08 16:40:25 +0100

    browse code, simple codebreaking

--- inc/templates/head.php
+++ inc/templates/head.php
@@ -126,7 +126,7 @@ if (isset($_POST['next'])) {
     <div class="container position-sticky z-index-sticky top-0">
         <div class="row">
             <div class="col-12">
-                <nav class="navbar navbar-expand-lg  blur border-radius-xl top-0 z-index-fixed shadow position-absolute my-3 py-2 start-0 end-0 mx-4">
+                <nav class="navbar navbar-expand-lg  blur border-radius-md top-0 z-index-fixed shadow position-absolute my-3 py-2 start-0 end-0 mx-4">
                     <div class="container-fluid px-0">
                         <a class="navbar-brand font-weight-bolder ms-sm-3" href="index.php" rel="tooltip" title="Designed and Coded by Sven Bledt" data-placement="bottom" target="_blank">
                             Trivia Quiz
